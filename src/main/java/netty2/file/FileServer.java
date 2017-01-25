fed8ca9800f68fa63b0cfdc8a0efc4c156cc4b6f
sunny <2623581340@qq.com>
2017-01-25 17:01:59 +0800

    犀利java： netty2 添加 file包

---
+++ src/main/java/netty2/file/FileServer.java
@@ -0,0 +1,56 @@
+package netty2.file;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ *
+ * Created by sunny-chen on 17/1/24.
+ */
+public class FileServer {
+
+    static final int PORT = 8023;
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup   = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel channel) throws Exception {
+                            ChannelPipeline pipeline = channel.pipeline();
+
+                            pipeline.addLast(
+                                    new StringEncoder(CharsetUtil.UTF_8),
+                                    new LineBasedFrameDecoder(8192),
+                                    new StringDecoder(CharsetUtil.UTF_8),
+                                    new ChunkedWriteHandler(),
+                                    new FileServerHandler());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.bind(PORT).sync();
+
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
