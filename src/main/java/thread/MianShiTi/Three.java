2d019000d3287537dd313c0834f399bbdf0fd254
sunny <2623581340@qq.com>
2017-05-18 23:04:30 +0800

    犀利Java : 线程学习

---
+++ src/main/java/thread/MianShiTi/Three.java
@@ -0,0 +1,12 @@
+package thread.MianShiTi;
+
+/**
+ * 设计四个线程,其中两个线程每次对变量i加1,另外两个线程每次对i减1
+ *
+ * Created by sunny on 2017/5/18.
+ */
+public class Three {
+
+
+
+}
--- src/main/java/thread/MianShiTi/Two.java
+++ src/main/java/thread/MianShiTi/Two.java
@@ -1,5 +1,6 @@
 package thread.MianShiTi;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,5 +15,68 @@ public class Two {
 
     Lock lock = new ReentrantLock();
 
+    Condition con = lock.newCondition();
 
+    public void sub() {
+        lock.lock();
+
+        try {
+            while (flag) {
+                try {
+                    con.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println("sub" + i);
+            }
+
+            flag = true;
+            con.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public synchronized void main() {
+        lock.lock();
+
+        try {
+            while (!flag) {
+                try {
+                    con.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            for (int i = 0; i < 20; i++) {
+                System.out.println("main" + i);
+            }
+
+            flag = false;
+            con.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        Two two = new Two();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 50; i++) {
+                    two.sub();
+                }
+            }
+        }).start();
+
+        for (int i = 0; i < 50; i++) {
+            two.main();
+        }
+    }
 }
